821252dc1619cc8d2b005f0626d582b1ec2f5929
github workflow <test@example.com>
2025-08-05 09:22:14 +0000

    297f2337925583d8ac8ab002de6d3da600501783

--- kabeuchi/script/script.js
+++ kabeuchi/script/script.js
@@ -1,107 +1,111 @@
 'use strict';
 
+// botの返信する内容
+let botReply = ["はい", "はい", "うん", "うん", ""];
 // ユーザーの回答内容の全てを記憶する配列
 let userData = [];
-
-// 一番下へ
-function chatToBottom() {
-	const chatField = document.getElementById('chatbot-body');
-	chatField.scroll(0, chatField.scrollHeight - chatField.clientHeight);
-}
 
 const userText = document.getElementById('chatbot-text');
 const chatSubmitBtn = document.getElementById('chatbot-submit');
 const chatDumpBtn = document.getElementById('chatbot-save');
-
-// 拡大ボタン
 const chatbot = document.getElementById('chatbot');
 const chatbotBody = document.getElementById('chatbot-body');
 const chatbotFooter = document.getElementById('chatbot-footer');
 
+userText.addEventListener("keydown", sendByCtrlEnter);
+chatSubmitBtn.addEventListener('click', displayUserOutputAndReply);
+chatDumpBtn.addEventListener('click', saveChatLog);
+
+robotOutput('よろしくお願いします。');
+
+//
+// -- ここから汎用の処理にしたい --
+//
+
+// 抽選
+function lot(data) {
+	return data[Math.floor(Math.random() * data.length)];
+}
+
+// 一番下までスクロール
+function scroll_element(e) {
+	e.scroll(0, e.scrollHeight - e.clientHeight);
+}
+
+//
+// ----------ここから関数 -------------
+//
+
+// Enterキーが押された時にSubmitされるのを抑制する
+function sendByCtrlEnter(event) {
+	if (event.key == 'Enter') {
+		if (event.ctrlKey) {
+			chatSubmitBtn.click();
+		} else {
+			event.preventDefault();
+			userText.value = userText.value + '\n';
+		}
+	}
+}
+
+function displayUserOutputAndReply() {
+	const txt = userText.value.trim();
+
+	const ret = userOutput(txt);
+	userText.value = '';
+	if (!ret) return;
+	userData.push(txt);
+	const rep = lot(botReply);
+	robotOutput(rep);
+}
+
 // 書き込んだ内容のセーブ
-chatDumpBtn.addEventListener('click', () => {
-	const blob = new Blob([userData.join("\n").concat(['\n'])], {type: "text/plain"});
+function saveChatLog() {
+	const blob = new Blob([userData.join("\n").concat(['\n'])], {
+		type: "text/plain"
+	});
 	const url = URL.createObjectURL(blob);
 	const a = document.createElement("a");
 	a.href = url;
 	a.download = "kabeuchi_log_" + Date.now() + ".txt";
 	a.click();
 	URL.revokeObjectURL(url);
-})
+}
 
-// --------------------自分の投稿（送信ボタンを押した時の処理）--------------------
-chatSubmitBtn.addEventListener('click', () => {
+// 投稿
+function post(text, is_me) {
+	if (!text) return false;
 
-	// 空行の場合送信不可
-	if (!userText.value || !userText.value.match(/\S/g)) return false;
+	const date = (new Date()).toLocaleString('ja-JP', {
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit'
+	});
+	const side = is_me ? 'right' : 'left';
 
-	// 投稿内容をSave用に保存
-	userData.push(userText.value);
-	console.log(userData);
-
-	// ulとliを作り、右寄せのスタイルを適用し投稿する
-	const ul = document.getElementById('chatbot-ul');
 	const li = document.createElement('li');
-	// このdivにテキストを指定
-	const div = document.createElement('div');
-	// 時間表示
-	const date = new Date();
-	const time=document.createElement('div');
+	const message = document.createElement('pre');
+	const time = document.createElement('div');
 
-	li.classList.add('right');
-	ul.appendChild(li);
-	li.appendChild(div);
-
-	div.classList.add('chatbot-right');
-	div.textContent = userText.value;
-
+	chatbotBody.appendChild(li);
+	li.appendChild(message);
 	li.appendChild(time);
+
+	li.classList.add(side);
+	message.classList.add('chatbot-' + side);
 	time.classList.add('chatbot-time');
-	time.textContent = date.toLocaleString('ja-JP', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
-	robotOutput(lot({ "はい": 50, "うん": 50, }));
+	message.textContent = text;
+	time.textContent = date;
 
-	// 一番下までスクロール
-	chatToBottom();
+	time.scrollIntoView(false);
+	return true;
+}
 
-	// テキスト入力欄を空白にする
-	userText.value = '';
-});
-
-function lot(data) {
-	const rand = Math.floor(Math.random() * 100)
-	let result = false
-	let rate = 0
-	for (const prop in data) {
-		rate += data[prop]
-		if (rand <= rate) {
-			result = prop
-			break
-		}
-	}
-	return result
+function userOutput(content) {
+	return post(content, true);
 }
 
 function robotOutput(content) {
-	if(lot({"a":66,"b":33}) == "b") return false;
-	chatSubmitBtn.disabled = true;
-
-	const ul = document.getElementById('chatbot-ul');
-	const li = document.createElement('li');
-	li.classList.add('left');
-	ul.appendChild(li);
-
-	// このdivにテキストを指定
-	const div = document.createElement('div');
-	li.appendChild(div);
-	div.classList.add('chatbot-left');
-	div.textContent = content;
-
-	// 一番下までスクロール
-	chatToBottom();
-
-	chatSubmitBtn.disabled = false;
+	return post(content, false);
 }
-
-// 初期画面
-robotOutput('よろしくお願いします。');
