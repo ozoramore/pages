0237acb457e29a00aafb3562ec96d6707f05b96f
github workflow <test@example.com>
2025-08-05 00:15:12 +0000

    9a4521971e12955172fb012d786f96053ce136df

--- kabeuchi/script/script.js
+++ kabeuchi/script/script.js
@@ -9,31 +9,57 @@ function chatToBottom() {
 	chatField.scroll(0, chatField.scrollHeight - chatField.clientHeight);
 }
 
+const userText = document.getElementById('chatbot-text');
 const chatSubmitBtn = document.getElementById('chatbot-submit');
+const chatDumpBtn = document.getElementById('chatbot-save');
+
+// 拡大ボタン
+const chatbot = document.getElementById('chatbot');
+const chatbotBody = document.getElementById('chatbot-body');
+const chatbotFooter = document.getElementById('chatbot-footer');
+
+// 書き込んだ内容のセーブ
+chatDumpBtn.addEventListener('click', () => {
+	const blob = new Blob([userData.join("\n").concat(['\n'])], {type: "text/plain"});
+	const url = URL.createObjectURL(blob);
+	const a = document.createElement("a");
+	a.href = url;
+	a.download = "kabeuchi_log_" + Date.now() + ".txt";
+	a.click();
+	URL.revokeObjectURL(url);
+})
 
 // --------------------自分の投稿（送信ボタンを押した時の処理）--------------------
 chatSubmitBtn.addEventListener('click', () => {
-	const userText = document.getElementById('chatbot-text');
+
 	// 空行の場合送信不可
 	if (!userText.value || !userText.value.match(/\S/g)) return false;
 
-	// 投稿内容を配列に保存しておく(ログ保存ボタンとか用意したい)
+	// 投稿内容をSave用に保存
 	userData.push(userText.value);
-	console.log(userText.value);
+	console.log(userData);
 
 	// ulとliを作り、右寄せのスタイルを適用し投稿する
 	const ul = document.getElementById('chatbot-ul');
 	const li = document.createElement('li');
 	// このdivにテキストを指定
 	const div = document.createElement('div');
+	// 時間表示
+	const date = new Date();
+	const time=document.createElement('div');
 
 	li.classList.add('right');
 	ul.appendChild(li);
 	li.appendChild(div);
+
 	div.classList.add('chatbot-right');
 	div.textContent = userText.value;
 
-	robotOutput();
+	li.appendChild(time);
+	time.classList.add('chatbot-time');
+	time.textContent = date.toLocaleString('ja-JP', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+
+	robotOutput(lot({ "はい": 50, "うん": 50, }));
 
 	// 一番下までスクロール
 	chatToBottom();
@@ -56,8 +82,8 @@ function lot(data) {
 	return result
 }
 
-function robotOutput() {
-	if(lot({"a":75,"b":25}) == "b") return false;
+function robotOutput(content) {
+	if(lot({"a":66,"b":33}) == "b") return false;
 	chatSubmitBtn.disabled = true;
 
 	const ul = document.getElementById('chatbot-ul');
@@ -69,14 +95,13 @@ function robotOutput() {
 	const div = document.createElement('div');
 	li.appendChild(div);
 	div.classList.add('chatbot-left');
-
-	div.textContent = lot({
-		"はい": 40,
-		"うん": 60,
-	});
+	div.textContent = content;
 
 	// 一番下までスクロール
 	chatToBottom();
 
 	chatSubmitBtn.disabled = false;
 }
+
+// 初期画面
+robotOutput('よろしくお願いします。');
