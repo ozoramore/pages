dc97904988cac114d7dfe2624fb3058d1d9a1abd
github workflows <email@example.com>
2025-08-29 00:14:52 +0000

    45a3c2257c061034cda97d3097c335cc3fc04596

--- kabeuchi/script.js
+++ kabeuchi/script.js
@@ -3,6 +3,7 @@
 // URLからParam取得
 var params = Object.fromEntries(new URLSearchParams(window.location.search));
 // botの返信する内容
+let initMessage = params.init == null? "" : params.first;
 let botReply = params.reply == null? ["はい", "うん"] : params.reply.split(',');
 // ユーザーの回答内容の全てを記憶する配列
 let userData = [];
@@ -28,7 +29,7 @@ chatDumpBtn.addEventListener('click', saveChatLog);
 chatbotMenu.addEventListener("click", openPopup);
 popupBackground.addEventListener("click", closePopup);
 
-robotOutput('よろしくお願いします。');
+robotOutput(initMessage);
 
 //
 // -- ここから汎用の処理にしたい --
